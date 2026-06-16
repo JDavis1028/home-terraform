baa02d47426d6e3e0913804d48ed7933fa60ef50
JDavis1028 <jackson_davis1028@icloud.com>
2026-06-16 16:06:00 -0400

    Deployed A vm for running postgres & redis

--- locals.vms.tf
+++ locals.vms.tf
@@ -1,25 +1,15 @@
 locals {
   vms = {
 
-    "pd1-vm-docker-1" = {
+    "pd1-vm-database" = {
       vmid        = 100
       target_node = "PVEProdesk1"
-      memory      = 3072
+      memory      = 4098
       cores       = 2
       disk0_size  = "6G"
       disk1_size  = "32G"
       ip          = "192.168.1.19/24"
     }
 
-    # "pd1-vm-docker-2" = {
-    #   vmid        = 102
-    #   target_node = "PVEProdesk1"
-    #   memory      = 3072
-    #   cores       = 2
-    #  disk1_size  = "32G"
-    #  ip = "192.168.1.16/24"
-    # }
-
   }
-
 }
